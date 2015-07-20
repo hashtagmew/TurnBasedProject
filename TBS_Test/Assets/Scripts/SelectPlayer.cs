6d03849ced5bda4f19e23466f4f55c1c2c3781de
Macenzie Allanson <111294@student.sae.edu.au>
2015-07-20 15:03:26 +1000

    Sliding unit on mouse down

--- TBS_Test/Assets/Scripts/SelectPlayer.cs
+++ TBS_Test/Assets/Scripts/SelectPlayer.cs
@@ -3,24 +3,32 @@ using System.Collections;
 
 public class SelectPlayer : MonoBehaviour {
 
-	public float speed = 1.5f;
-	private Vector3 target;
+	public float fSpeed = 1.5f;
+	private Vector3 vTarget;
+
+	public TileCursor Cursor;
+	public MapTile tileGoal;
 
 	bool bUnitSelected;
 	// Use this for initialization
 	void Start () {
-		target = transform.position;
+		vTarget = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0))
 		{
+
 			Debug.Log("Mouse is down");
-			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			target.z = transform.position.z;
+			//vTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			//vTarget.z = transform.position.z;
+			if (Cursor.mapTile != null) {
+				vTarget = Cursor.mapTile.gameObject.transform.position;
+				vTarget = new Vector3(vTarget.x, 0.1f, vTarget.z);
+			}
 		}
-		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards(transform.position, vTarget, fSpeed * Time.deltaTime);
 	}
 
 	void OnClick(){
