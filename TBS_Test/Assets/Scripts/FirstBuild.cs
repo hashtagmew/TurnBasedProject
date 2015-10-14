fe07076b86d359bc455bd4bffe2d4ca04938f12b
Kouta <kouta@koutacles.com.au>
2015-10-14 16:14:43 +1100

    +First audio assets +Loading a level and trying to generate an A* map

---
+++ TBS_Test/Assets/Scripts/FirstBuild.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstBuild : MonoBehaviour {
+
+	public AudioClip testsound1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+			this.GetComponent<AudioSource>().PlayOneShot(testsound1);
+		}
+
+		if (Input.GetKeyDown(KeyCode.F1)) {
+			GameObject.FindObjectOfType<GameMap>().LoadMap("Assets/Resources/Maps/testfeatures.xml");
+		}
+	}
+}
--- TBS_Test/Assets/Scripts/Terrain/GameMap.cs
+++ TBS_Test/Assets/Scripts/Terrain/GameMap.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
 public class GameMap : MonoBehaviour {
 	public GameObject TilePrefab;
 
@@ -12,6 +16,16 @@ public class GameMap : MonoBehaviour {
 	List <List<MapTile>> map = new List<List<MapTile>>();
 
 	public bool bGridToggle = false;
+
+	static private XDocument s_xmlDoc;
+
+	public GameObject goProtoTile;
+	public GameObject goProtoFeature;
+	public GameObject goProtoLight;
+
+	public string sName = "Map";
+	public string sDescription = "";
+
 
 	void Start() {
 		GenerateMap();  
@@ -24,6 +38,116 @@ public class GameMap : MonoBehaviour {
 		}
 	}
 
+	public void Resize(int rows, int cols) {
+		iMapHorzSize = rows;
+		iMapVertSize = cols;
+	}
+
+	public void LoadMap(string path) {
+		s_xmlDoc = XDocument.Load(path);
+		
+		int tempcols = 0;
+		int temprows = 0;
+
+		//Delete old map
+		GameObject goTileCheck;
+
+		for (int i = 0; i < iMapVertSize; i++) {
+			for (int j = 0; j < iMapHorzSize; j++) {
+				goTileCheck = GameObject.Find(string.Format("Tile_{0}_{1}", i, j));
+				if (goTileCheck != null) {
+					GameObject.Destroy(goTileCheck);
+				}
+			}
+		}
+		
+		foreach (XElement xroot in s_xmlDoc.Elements()) {
+			foreach (XElement xlayer1_properties in xroot.Elements()) {
+				if (xlayer1_properties.Name == "name") {
+					sName = xlayer1_properties.Value;
+				}
+				else if (xlayer1_properties.Name == "description") {
+					sDescription = xlayer1_properties.Value;
+				}
+				else if (xlayer1_properties.Name == "mapsizex") {
+					temprows = int.Parse(xlayer1_properties.Value);
+				}
+				else if (xlayer1_properties.Name == "mapsizey") {
+					tempcols = int.Parse(xlayer1_properties.Value);
+				}
+				
+				if (xlayer1_properties.Name == "lights") {
+					foreach (XElement xlayer2_tiles in xlayer1_properties.Elements()) {
+						GameObject TempLightObj = GameObject.Instantiate(goProtoLight);
+						Light TempLight = TempLightObj.GetComponent<Light>();
+						TempLight.tag = "MapLight";
+						
+						foreach (XElement xlayer3_tiledata in xlayer2_tiles.Elements()) {
+							if (xlayer3_tiledata.Name == "type") {
+								TempLight.type = (LightType)int.Parse(xlayer3_tiledata.Value);
+							}
+							else if (xlayer3_tiledata.Name == "positionx") {
+								TempLight.transform.position = new Vector3(float.Parse(xlayer3_tiledata.Value), TempLight.transform.position.y, TempLight.transform.position.z);
+							}
+							else if (xlayer3_tiledata.Name == "positiony") {
+								TempLight.transform.position = new Vector3(TempLight.transform.position.x, float.Parse(xlayer3_tiledata.Value), TempLight.transform.position.z);
+							}
+							else if (xlayer3_tiledata.Name == "positionz") {
+								TempLight.transform.position = new Vector3(TempLight.transform.position.x, TempLight.transform.position.y, float.Parse(xlayer3_tiledata.Value));
+							}
+							else if (xlayer3_tiledata.Name == "range") {
+								TempLight.range = float.Parse(xlayer3_tiledata.Value);
+							}
+							else if (xlayer3_tiledata.Name == "intensity") {
+								TempLight.intensity = float.Parse(xlayer3_tiledata.Value);
+							}
+							else if (xlayer3_tiledata.Name == "color") {
+								Color TempColor = Color.black;
+								Color.TryParseHexString(xlayer3_tiledata.Value, out TempColor);
+								TempLight.color = TempColor;
+							}
+						}
+					}
+				}
+				
+				if (xlayer1_properties.Name == "tiles") {
+					Resize(temprows, tempcols);
+					foreach (XElement xlayer2_tiles in xlayer1_properties.Elements()) {
+						GameObject TempTile = GameObject.Instantiate(goProtoTile);
+						TempTile.transform.SetParent(transform);
+						
+						foreach (XElement xlayer3_tiledata in xlayer2_tiles.Elements()) {
+							if (xlayer3_tiledata.Name == "type") {
+								TempTile.GetComponent<MapTile>().Terraform((TERRAIN_TYPE)int.Parse(xlayer3_tiledata.Value));
+							}
+							else if (xlayer3_tiledata.Name == "xpos") {
+								TempTile.GetComponent<MapTile>().vGridPosition = new Vector2(int.Parse(xlayer3_tiledata.Value), TempTile.GetComponent<MapTile>().vGridPosition.y);
+							}
+							else if (xlayer3_tiledata.Name == "ypos") {
+								TempTile.GetComponent<MapTile>().vGridPosition = new Vector2(TempTile.GetComponent<MapTile>().vGridPosition.x, int.Parse(xlayer3_tiledata.Value));
+							}
+							else if (xlayer3_tiledata.Name == "feature") {
+								GameObject tempobj = GameObject.Instantiate(goProtoFeature);
+								TempTile.GetComponent<MapTile>().l_tfFeatures.Add(tempobj.GetComponent<TerrainFeature>());
+								
+								tempobj.transform.SetParent(TempTile.transform);
+								tempobj.transform.localRotation = TempTile.transform.localRotation;
+								int value = int.Parse(xlayer3_tiledata.Attribute("type").Value);
+								tempobj.GetComponent<TerrainFeature>().Terraform((FEATURE_TYPE)value);
+								tempobj.transform.localPosition = new Vector3(0.0f, 0.0f, (tempobj.transform.localScale.z / 2) * -1);
+							}
+						}
+						
+						TempTile.name = string.Format("Tile_{0}_{1}", TempTile.GetComponent<MapTile>().vGridPosition.x, TempTile.GetComponent<MapTile>().vGridPosition.y);
+						TempTile.transform.localPosition = new Vector3(TempTile.GetComponent<MapTile>().vGridPosition.x + 0.5f, TempTile.GetComponent<MapTile>().vGridPosition.y + 0.5f, 0);
+						TempTile.transform.localRotation = Quaternion.identity;
+						TempTile.transform.localScale = new Vector3(1, 1, 0.1f);
+					}
+				}
+			}
+		}
+	}
+
 	public void GenerateMap() {
 		map = new List<List<MapTile>>();
 		for (int i = 0; i < iMapHorzSize; i++) {
@@ -33,7 +157,7 @@ public class GameMap : MonoBehaviour {
 				                                        new Vector3(i * fTileSize, 0, j * -fTileSize),
 				                                         Quaternion.identity)).GetComponent<MapTile>();
 				tile.vGridPosition = new Vector2(i, j);
-				tile.gameObject.name = "Tile (" + i.ToString("D2") + ", " + j.ToString("D2") + ")";
+				tile.gameObject.name = "Tile_" + i.ToString() + "_" + j.ToString();
 				tile.transform.parent = this.gameObject.transform;
 				row.Add(tile);
 
