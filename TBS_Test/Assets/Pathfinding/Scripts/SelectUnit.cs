1fe925153f217770634efb0ead09ecac72fbc372
Kouta <kouta@koutacles.com.au>
2015-10-29 10:09:57 +1100

    +Selection UI in test scene

--- TBS_Test/Assets/Pathfinding/Scripts/SelectUnit.cs
+++ TBS_Test/Assets/Pathfinding/Scripts/SelectUnit.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
+using UnityEngine.UI;
+
 public class SelectUnit : MonoBehaviour {
 
 	public TileMap map;
@@ -11,11 +13,22 @@ public class SelectUnit : MonoBehaviour {
 	Ray ray;
 	RaycastHit rayHit;
 
+	public Text txtSelectedName;
+	public Text txtSelectedHP;
+	//public Text txtSelectedAP;
+	public Text txtSelectedPhysAttack;
+	public Text txtSelectedRangAttack;
+	public Text txtSelectedMagiAttack;
+	public Text txtSelectedDefence;
+	public Text txtSelectedResistance;
+
+	public UIManager uiMng;
+
 	void Start () {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 	}
 
-	void Update (){
+	void Update () {
 		if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
 			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -27,5 +40,30 @@ public class SelectUnit : MonoBehaviour {
 
 			}
 		}
+
+		if (map.selectedUnit == null) {
+			txtSelectedName.text = "None";
+			txtSelectedHP.text = "";
+			txtSelectedPhysAttack.text = "";
+			txtSelectedRangAttack.text = "";
+			txtSelectedMagiAttack.text = "";
+			txtSelectedDefence.text = "";
+			txtSelectedResistance.text = "";
+
+			bSelected = false;
+		}
+		else {
+			txtSelectedName.text = map.selectedUnit.GetComponent<GameUnit>().sName;
+			txtSelectedHP.text = map.selectedUnit.GetComponent<GameUnit>().fHealth.ToString() + "/\n" + map.selectedUnit.GetComponent<GameUnit>().fMaxHealth.ToString();
+			txtSelectedPhysAttack.text = map.selectedUnit.GetComponent<GameUnit>().fPhysAttack.ToString();
+			txtSelectedRangAttack.text = map.selectedUnit.GetComponent<GameUnit>().fRangAttack.ToString();
+			txtSelectedMagiAttack.text = map.selectedUnit.GetComponent<GameUnit>().fMagiAttack.ToString();
+			txtSelectedDefence.text = map.selectedUnit.GetComponent<GameUnit>().fDefence.ToString();
+			txtSelectedResistance.text = map.selectedUnit.GetComponent<GameUnit>().fResistance.ToString();
+
+			bSelected = true;
+		}
+
+		uiMng.bUnitShow = bSelected;
 	}
 }
