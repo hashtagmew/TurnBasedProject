e301ff6717bdbf90f0dbebdb87f1883106ed11b2
Macenzie Allanson <111294@student.sae.edu.au>
2015-10-28 20:47:11 +1100

    Path finding now moved successfully  into persp-test scene in TB_Test folder.

---
+++ TBS_Test/Assets/Pathfinding/Scripts/SelectUnit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SelectUnit : MonoBehaviour {
+
+	public TileMap map;
+	//public Unit unit;
+	public bool bSelected = false;
+
+	Ray ray;
+	RaycastHit rayHit;
+
+	void Start () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+	}
+
+	void Update (){
+		if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+			if (Physics.Raycast(ray, out rayHit, Mathf.Infinity, 1 << LayerMask.NameToLayer("GameUnit"))) {
+				Debug.DrawLine(ray.GetPoint(0), rayHit.point, Color.green);
+				map.selectedUnit = rayHit.collider.transform.parent.gameObject;
+				Debug.Log("Unit Selected: " + map.selectedUnit.name);
+
+			}
+		}
+	}
+}
