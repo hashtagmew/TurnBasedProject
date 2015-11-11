e6096a6b89e43d54d34e3b852c3db9ca7c844ad1
unknown <mallanson@SYD-1231-W.sae.au>
2015-11-12 10:49:32 +1100

    Movement Cost added Will reset if you click to end turn

---
+++ TBS_Test/Assets/Pathfinding/Scripts/UnitMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitMovement : MonoBehaviour {
+
+	public float remainingMovement;
+	public float resetMovement;
+
+	// Use this for initialization
+	void Start () {
+		remainingMovement = remainingMovement;
+		resetMovement = resetMovement;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
