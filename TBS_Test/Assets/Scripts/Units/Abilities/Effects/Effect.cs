9b3daa9d5c0ad88298474cd2356a1185f7cbdb88
Kouta <kouta@koutacles.com.au>
2015-12-10 15:28:52 +1100

    +Final Audio +Developer tools

--- TBS_Test/Assets/Scripts/Units/Abilities/Effects/Effect.cs
+++ TBS_Test/Assets/Scripts/Units/Abilities/Effects/Effect.cs
@@ -8,14 +8,17 @@ public class Effect {
 	public string sName = "Nothing";
 	public EFFECT_TYPE iType = EFFECT_TYPE.NONE;
 	public bool bAdjustable = false;
+	public string sAdjustName = "";
+	public float fAdjustFloat = 0.0f;
 
 	public Effect() {
 		//
 	}
 
-	public Effect(string name, EFFECT_TYPE type, bool adjustable) {
+	public Effect(string name, EFFECT_TYPE type, bool adjustable, string adjustname = "none") {
 		sName = name;
 		iType = type;
 		bAdjustable = adjustable;
+		sAdjustName = adjustname;
 	}
 }
--- TBS_Test/Assets/Scripts/Units/Abilities/Effects/EffectBox.cs
+++ TBS_Test/Assets/Scripts/Units/Abilities/Effects/EffectBox.cs
@@ -8,20 +8,43 @@ public static class EffectBox {
 	
 	static EffectBox() {
 		s_dEffectLookup = new Dictionary<string, Effect>();
+
+		//Damage
+		s_dEffectLookup.Add("Damage", new Effect("Damage", EFFECT_TYPE.RESOLUTION, true, "Amount"));
+		s_dEffectLookup.Add("Heal", new Effect("Heal", EFFECT_TYPE.RESOLUTION, true, "Amount"));
+
+		//Passive Buffs
+		s_dEffectLookup.Add("Defence Buff", new Effect("Defence Buff", EFFECT_TYPE.PASSIVE, true, "Amount"));
+
+		//Terrain Manipulation
+		s_dEffectLookup.Add("Push Mountain", new Effect("Push Mountain", EFFECT_TYPE.RESOLUTION, true, "Tiles"));
+		s_dEffectLookup.Add("Summon Wall", new Effect("Summon Wall", EFFECT_TYPE.RESOLUTION, false));
+
+		//Sprite Effects
+		s_dEffectLookup.Add("Hide Sprite", new Effect("Hide Sprite", EFFECT_TYPE.ACTIVATION, false));
+		s_dEffectLookup.Add("Show Sprite", new Effect("Show Sprite", EFFECT_TYPE.RESOLUTION, false));
+
+		//Movement
+		s_dEffectLookup.Add("Teleport Caster to Target", new Effect("Teleport", EFFECT_TYPE.RESOLUTION, false));
+
+		//Camera
+		s_dEffectLookup.Add("Pan Camera to Target (A)", new Effect("PanCameraTargetA", EFFECT_TYPE.ACTIVATION, true, "Pan Speed"));
+		s_dEffectLookup.Add("Pan Camera to Target (R)", new Effect("PanCameraTargetR", EFFECT_TYPE.RESOLUTION, true, "Pan Speed"));
+
+		s_dEffectLookup.Add("Pan Camera to Caster (A)", new Effect("PanCameraCasterA", EFFECT_TYPE.ACTIVATION, true, "Pan Speed"));
+		s_dEffectLookup.Add("Pan Camera to Caster (R)", new Effect("PanCameraCasterR", EFFECT_TYPE.RESOLUTION, true, "Pan Speed"));
+
+		s_dEffectLookup.Add("Teleport Camera to Target (A)", new Effect("TeleportCameraTargetA", EFFECT_TYPE.ACTIVATION, false));
+		s_dEffectLookup.Add("Teleport Camera to Target (R)", new Effect("TeleportCameraTargetR", EFFECT_TYPE.RESOLUTION, false));
 		
-		s_dEffectLookup.Add("Target Self", new Effect("Target Self", EFFECT_TYPE.ACTIVATION, false));
-		s_dEffectLookup.Add("Target Other", new Effect("Target Other", EFFECT_TYPE.ACTIVATION, true));
-		s_dEffectLookup.Add("Target Area", new Effect("Target Area", EFFECT_TYPE.ACTIVATION, true));
+		s_dEffectLookup.Add("Teleport Camera to Caster (A)", new Effect("TeleportCameraCasterA", EFFECT_TYPE.ACTIVATION, false));
+		s_dEffectLookup.Add("Teleport Camera to Caster (R)", new Effect("TeleportCameraCasterR", EFFECT_TYPE.RESOLUTION, false));
 
-		s_dEffectLookup.Add("Damage Target", new Effect("Damage Target", EFFECT_TYPE.RESOLUTION, true));
-		s_dEffectLookup.Add("Heal Target", new Effect("Heal Target", EFFECT_TYPE.RESOLUTION, true));
+		//Races
+		s_dEffectLookup.Add("(Race) Magical", new Effect("Magical", EFFECT_TYPE.PASSIVE, false));
+		s_dEffectLookup.Add("(Race) Mechanical", new Effect("Mechanical", EFFECT_TYPE.PASSIVE, false));
 
-		s_dEffectLookup.Add("Resist Damage", new Effect("Resist Damage", EFFECT_TYPE.PASSIVE, true));
-		s_dEffectLookup.Add("Defence Buff", new Effect("Defence Buff", EFFECT_TYPE.PASSIVE, true));
-
-		s_dEffectLookup.Add("(Race) Human", new Effect("Human", EFFECT_TYPE.PASSIVE, false));
-		s_dEffectLookup.Add("(Race) Machine", new Effect("Machine", EFFECT_TYPE.PASSIVE, false));
-
+		//Types
 		s_dEffectLookup.Add("(Unit Type) Infantry", new Effect("Infantry", EFFECT_TYPE.PASSIVE, false));
 		s_dEffectLookup.Add("(Unit Type) Support", new Effect("Support", EFFECT_TYPE.PASSIVE, false));
 	}
