38e6551638771e66f206f57abd43f109e732a970
unknown <mallanson@SYD-1269-W.sae.au>
2015-12-11 12:46:34 +1100

    change to NetGameUnit

--- TBS_Test/Assets/Scripts/Units/GameUnit.cs
+++ TBS_Test/Assets/Scripts/Units/GameUnit.cs
@@ -117,94 +117,94 @@ public class GameUnit : Photon.MonoBehaviour, ISelectable {
 			transform.position = Vector3.Lerp (transform.position, this.vCorrectPos, Time.deltaTime);
 			transform.rotation = Quaternion.Lerp (transform.rotation, this.qCorrectRot, Time.deltaTime);
 		} else {
-			Debug.Log("nullPhoton");
-		}
+			Debug.Log ("nullPhoton");
 
 
-		//End turn
+		
+
+
+			//End turn
 		
 		
-		//Finding Direction the Unit is moving in so that the sprite can be changed
+			//Finding Direction the Unit is moving in so that the sprite can be changed
 		
-		if (currentPath != null && currentPath.Count > 1) {
-			CurPos = new Vector2(tileX, tileY);
-			NextPos = new Vector2 (currentPath[1].x, currentPath [1].y);
-		}
-		
-		currDirX = CurPos.x - NextPos.x;
-		currDirY = CurPos.y - NextPos.y;
-		
-		//setting direction
-		if (currDirX == -1 && currDirY == 0) {
-			eGridDirection = UNIT_DIR.UP_RIGHT;
-		}
-		else if (currDirX == 0 && currDirY == 1) {
-			eGridDirection = UNIT_DIR.DOWN_RIGHT;
-		}
-		else if (currDirX == 0 && currDirY == -1) {
-			eGridDirection = UNIT_DIR.UP_LEFT;
-		}
-		else if (currDirX == 1 && currDirY == 0) {
-			eGridDirection = UNIT_DIR.DOWN_LEFT;
-		}
-		
-		//Changing sprite based on direction
-		if (eGridDirection == UNIT_DIR.UP_RIGHT) {
-			myRend.sprite = texDirSpriteUR;
-		}
-		if (eGridDirection == UNIT_DIR.DOWN_RIGHT) {
-			myRend.sprite = texDirSpriteDR;
-		}
-		if (eGridDirection == UNIT_DIR.UP_LEFT) {
-			myRend.sprite = texDirSpriteUL;
-		}
-		if (eGridDirection == UNIT_DIR.DOWN_LEFT) {
-			myRend.sprite = texDirSpriteDL;
-		}
-		
-		// Draw our debug line showing the pathfinding!
-		// NOTE: This won't appear in the actual game view.
-		if(currentPath != null) {
-			int currNode = 0;
-			
-			while( currNode < currentPath.Count-1 ) {
-				
-				Vector3 start = map.TileCoordToWorldCoord( currentPath[currNode].x, currentPath[currNode].y ) + 
-					new Vector3(0, -0.5f, 0) ;
-				Vector3 end   = map.TileCoordToWorldCoord( currentPath[currNode+1].x, currentPath[currNode+1].y )  + 
-					new Vector3(0, -0.5f, 0) ;
-				
-				Debug.DrawLine(start, end, Color.red);
-				
-				currNode++;
+			if (currentPath != null && currentPath.Count > 1) {
+				CurPos = new Vector2 (tileX, tileY);
+				NextPos = new Vector2 (currentPath [1].x, currentPath [1].y);
 			}
+		
+			currDirX = CurPos.x - NextPos.x;
+			currDirY = CurPos.y - NextPos.y;
+		
+			//setting direction
+			if (currDirX == -1 && currDirY == 0) {
+				eGridDirection = UNIT_DIR.UP_RIGHT;
+			} else if (currDirX == 0 && currDirY == 1) {
+				eGridDirection = UNIT_DIR.DOWN_RIGHT;
+			} else if (currDirX == 0 && currDirY == -1) {
+				eGridDirection = UNIT_DIR.UP_LEFT;
+			} else if (currDirX == 1 && currDirY == 0) {
+				eGridDirection = UNIT_DIR.DOWN_LEFT;
+			}
+		
+			//Changing sprite based on direction
+			if (eGridDirection == UNIT_DIR.UP_RIGHT) {
+				myRend.sprite = texDirSpriteUR;
+			}
+			if (eGridDirection == UNIT_DIR.DOWN_RIGHT) {
+				myRend.sprite = texDirSpriteDR;
+			}
+			if (eGridDirection == UNIT_DIR.UP_LEFT) {
+				myRend.sprite = texDirSpriteUL;
+			}
+			if (eGridDirection == UNIT_DIR.DOWN_LEFT) {
+				myRend.sprite = texDirSpriteDL;
+			}
+		
+			// Draw our debug line showing the pathfinding!
+			// NOTE: This won't appear in the actual game view.
+			if (currentPath != null) {
+				int currNode = 0;
+			
+				while (currNode < currentPath.Count-1) {
+				
+					Vector3 start = map.TileCoordToWorldCoord (currentPath [currNode].x, currentPath [currNode].y) + 
+						new Vector3 (0, -0.5f, 0);
+					Vector3 end = map.TileCoordToWorldCoord (currentPath [currNode + 1].x, currentPath [currNode + 1].y) + 
+						new Vector3 (0, -0.5f, 0);
+				
+					Debug.DrawLine (start, end, Color.red);
+				
+					currNode++;
+				}
+			}
+		
+			//		//Reset Movement Cost
+			//		if (currentPath == null && remainingMovement <= 0) {
+			//			remainingMovement = resetMovement;
+			//		}
+			//		//Make sure unit stops after remaining steps has been achieved
+			//		if (remainingMovement == 0) {
+			//			currentPath = null;
+			//			remainingMovement = resetMovement;
+			//		}
+		
+			// Have we moved our visible piece close enough to the target tile that we can
+			// advance to the next step in our pathfinding?
+			//Debug.Log (transform.position);
+			if (map == null) {
+				Debug.Log ("OH GOID");
+			} else {
+				//Debug.Log ("YEA");
+			}
+		
+			if (Vector3.Distance (transform.position, map.TileCoordToWorldCoord (tileX, tileY)) < 0.1f) {
+				AdvancePathing ();
+			}
+		
+			// Smoothly animate towards the correct map tile.
+			transform.position = Vector3.Lerp (transform.position, map.TileCoordToWorldCoord (tileX, tileY), 5f * Time.deltaTime);
 		}
-		
-		//		//Reset Movement Cost
-		//		if (currentPath == null && remainingMovement <= 0) {
-		//			remainingMovement = resetMovement;
-		//		}
-		//		//Make sure unit stops after remaining steps has been achieved
-		//		if (remainingMovement == 0) {
-		//			currentPath = null;
-		//			remainingMovement = resetMovement;
-		//		}
-		
-		// Have we moved our visible piece close enough to the target tile that we can
-		// advance to the next step in our pathfinding?
-		//Debug.Log (transform.position);
-		if (map == null) {
-			Debug.Log ("OH GOID");
-		} else {
-			//Debug.Log ("YEA");
-		}
-		
-		if (Vector3.Distance (transform.position, map.TileCoordToWorldCoord (tileX, tileY)) < 0.1f) {
-			AdvancePathing();
-		}
-		
-		// Smoothly animate towards the correct map tile.
-		transform.position = Vector3.Lerp(transform.position, map.TileCoordToWorldCoord( tileX, tileY ), 5f * Time.deltaTime);
 	}
 
 	// Advances our pathfinding progress by one tile.
